8a6fbce6bc2f111c89c2dc538f7e33a9246c8854
2uy2 <quytran11204@gmail.com>
2025-11-20 23:38:53 +0700

    xoá 1 công việc

--- api/v1/controller/task_controller.js
+++ api/v1/controller/task_controller.js
@@ -132,14 +132,14 @@ module.exports.changeMulti = async (req, res) => {
 
 }
 //posst /api/v1/tasks/create
-module.exports.create=async(req,res)=>{
+module.exports.create = async (req, res) => {
     try {
         const task = new Task(req.body);
         const data = await task.save();
         res.json({
-            code:200,
-            messeage:"tạo thành công",
-            data:data
+            code: 200,
+            messeage: "tạo thành công",
+            data: data
         })
     } catch (error) {
         res.json({
@@ -149,18 +149,40 @@ module.exports.create=async(req,res)=>{
     }
 }
 //posst /api/v1/tasks/edit/:id
-module.exports.edit=async(req,res)=>{
+module.exports.edit = async (req, res) => {
     try {
         const id = req.params.id;
         await Task.updateOne({
-            _id:id
-        },req.body);
-         res.json({
-            code:200,
-            messeage:"cập nhật thành công"
+            _id: id
+        }, req.body);
+        res.json({
+            code: 200,
+            messeage: "cập nhật thành công"
         })
     } catch (error) {
-         res.json({
+        res.json({
+            code: 400,
+            messeage: "lỗi"
+        });
+    }
+}
+
+//delete  /api/v1/tasks/delete/:id
+module.exports.delete = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Task.updateOne({
+            _id: id
+        },{
+            deleted:true,
+            deletedAt: new Date()
+        })
+        res.json({
+            code: 200,
+            messeage: "xoá thành công"
+        })
+    } catch (error) {
+        res.json({
             code: 400,
             messeage: "lỗi"
         });
