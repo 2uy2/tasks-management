edfb272926b06afd3d3facae8065f139c698d816
2uy2 <quytran11204@gmail.com>
2025-11-20 15:12:31 +0700

    bộ lọc trạng thái

--- api/v1/routes/task_route.js
+++ api/v1/routes/task_route.js
@@ -3,13 +3,20 @@ const router = express.Router();
 const Task=require("../../../model/task_model");
 // const controller = require("../../controllers/admin/task_controller");
 
+// get /api/v1/task
 router.get("/",async(req,res)=>{
-    const tasks = await Task.find({
+    const find = {
         deleted:false
-    })
-    console.log(tasks)
+    }
+    console.log(req.query);
+    if (req.query.status){
+        find.status=req.query.status
+    }
+    const tasks = await Task.find(find)
+    
     res.json(tasks);
 });
+//get api/v1/tasks/detail/:id
 router.get("/detail/:id",async(req,res)=>{
     try {
         const id = req.params.id;
