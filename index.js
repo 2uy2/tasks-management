821ca8d923f8509d0540285f8401ed559fefdf99
2uy2 <quytran11204@gmail.com>
2025-11-22 00:48:15 +0700

    cài đặt cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const database = require("./config/database");
 const bodyParser = require('body-parser'); // dùng để giúp đọc và hiểu dữ liệu mà client gửi lên server. ở dạng json (req.body)
+var cors = require('cors');
 require("dotenv").config();
 
 
 const routesApiVer1 = require("./api/v1/routes/index_route")
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors())
 
 database.connect();
 
