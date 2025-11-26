a82ebeebdc392c731bf2f0d1ae8f3924e1496210
2uy2 <quytran11204@gmail.com>
2025-11-26 18:30:07 +0700

    create vercel json

--- api/v1/model/task_model.js
+++ api/v1/model/task_model.js
@@ -8,6 +8,7 @@ const taskSchema = new mongoose.Schema({
     timeFinish:Date,
     createdBy:String,
     listUser:Array,
+    taskParentId:String,
     deleted :{
         type:Boolean,
         default:false // giá trị mặc định nếu người ta k xét thì sẽ là false 
